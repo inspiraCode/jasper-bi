0615b8bdd70cf915226cf3df1a7f02d4775d0e74
diego.torres.fuerte@gmail.com <diego.torres.fuerte@gmail.com@ad1d3ce3-a1f8-4f57-5b27-18f673554932>
2014-06-08 00:31:37 +0000

    Removing Eddy's folders.

--- trunk/DataMining/WSAdminPaqWrapper/WSAdminPaqWrapper/Process/ETLSellers.cs
+++ trunk/DataMining/WSAdminPaqWrapper/WSAdminPaqWrapper/Process/ETLSellers.cs
@@ -24,8 +24,31 @@ namespace WSAdminPaqWrapper.Process
         }
 
         private static void UpdateDim(string empresa, CatSeller cat, DimSellers dim, NpgsqlConnection conn)
-        { 
+        {
+            NpgsqlCommand cmd;
 
+            string sqlString = "UPDATE dim_sellers " +
+                "SET agent_code=@codigo, " +
+                "agent_name=@nombre " +
+                "WHERE ap_id=@id " +
+                "AND id_empresa=@empresa;";
+
+
+            cmd = new NpgsqlCommand(sqlString, conn);
+
+            cmd.Parameters.Add("@codigo", NpgsqlTypes.NpgsqlDbType.Varchar, 11);
+            cmd.Parameters.Add("@nombre", NpgsqlTypes.NpgsqlDbType.Varchar, 250);
+            cmd.Parameters.Add("@local", NpgsqlTypes.NpgsqlDbType.Boolean);
+            cmd.Parameters.Add("@empresa", NpgsqlTypes.NpgsqlDbType.Varchar, 150);
+            cmd.Parameters.Add("@id", NpgsqlTypes.NpgsqlDbType.Integer);
+
+            cmd.Parameters["@codigo"].Value = cat.CodigoCliente;
+            cmd.Parameters["@nombre"].Value = cat.RazonSocial;
+            cmd.Parameters["@local"].Value = cat.EsLocal;
+            cmd.Parameters["@empresa"].Value = dim.Empresa;
+            cmd.Parameters["@id"].Value = dim.IdCliente;
+
+            cmd.ExecuteNonQuery();
         }
 
         private static void LoadDim(string empresa, CatSeller cat, DimSellers dim, NpgsqlConnection conn)
