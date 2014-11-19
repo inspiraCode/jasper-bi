c446fe1cb310d31a515f36f2c6988d47aed6b07a
diego.torres.fuerte@gmail.com <diego.torres.fuerte@gmail.com@ad1d3ce3-a1f8-4f57-5b27-18f673554932>
2014-11-19 18:34:52 +0000

    alerts for accounts collectable.

--- trunk/fixed_data.sql
+++ trunk/fixed_data.sql
@@ -1,8 +1,6 @@
-INSERT INTO dim_grupo_vencimiento(minimo_dias, maximo_dias)
-VALUES(0,15);
-INSERT INTO dim_grupo_vencimiento(minimo_dias, maximo_dias)
-VALUES(16,30);
-INSERT INTO dim_grupo_vencimiento(minimo_dias, maximo_dias)
-VALUES(31,45);
-INSERT INTO dim_grupo_vencimiento(minimo_dias, maximo_dias)
-VALUES(46,0);
+INSERT INTO dim_grupo_vencimiento (minimo_dias, maximo_dias, a_vencidos, a_por_vencer)
+VALUES(0, 30, TRUE, FALSE);
+INSERT INTO dim_grupo_vencimiento (minimo_dias, maximo_dias, a_vencidos, a_por_vencer)
+VALUES(31, 0, TRUE, FALSE);
+INSERT INTO dim_grupo_vencimiento (minimo_dias, maximo_dias, a_vencidos, a_por_vencer)
+VALUES(0,6, FALSE, TRUE);
